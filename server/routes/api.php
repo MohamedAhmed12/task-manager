11714a3e8e06c1c2b4bc6448e587e89ce8496121
m_gad <mgad@unifonic.com>
2025-04-25 11:23:43 +0300

    feat(task-server): create task model, controller and APIs to handle CRUD ops

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,11 +1,17 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// auth APIs
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 Route::get('user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+
+
+// task APIs
+Route::apiResource('tasks', TaskController::class);
